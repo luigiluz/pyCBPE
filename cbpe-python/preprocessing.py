025530cdfac2a363fbe1b53e605835a4ca5f1aef
Luigi Luz <lfml@cesar.org.br>
2020-12-08 22:47:55 -0300

    Add preallocation in preprocessing module

--- cbpe-python/preprocessing.py
+++ cbpe-python/preprocessing.py
@@ -17,6 +17,7 @@ def _remove_high_frequency_components(signal):
     max_ripple = 0.5
     cut_off_freq_hz = 10
     cut_off_freq_rad_smp = (2 * cut_off_freq)/sampling_freq
+    low_pass_filtered_signal = np.zeros(length(signal))
 
     cheby_num, cheby_den = sp.signal.cheby1(filter_order, max_ripple, cut_off_freq_rad_smp, btype='lowpass')
     low_pass_filtered_signal = sp.signal.filtfilt(cheby_num, cheby_den, signal)
@@ -28,11 +29,12 @@ def _remove_baseline_wander(signal):
     signal_length = len(signal)
     first_window_size = np.floor(0.7 * signal_length)
     second_window_size = np.floor(0.3 * signal_length)
+    detrended_signal = np.zeros(signal_length)
 
-    first_window_output = sp.signal.medfilt(signal, first_window_size)
-    second_window_output = sp.signal.medfilt(first_window_output, second_window_size)
+    detrended_signal = sp.signal.medfilt(signal, first_window_size)
+    detrended_signal = sp.signal.medfilt(detrended_signal, second_window_size)
 
-    return second_window_output
+    return detrended_signal
 
 
 def _upsample(signal):
@@ -41,6 +43,7 @@ def _upsample(signal):
     desired_sampling_freq = 500
 
     desired_n_of_samples = (desired_sampling_freq * signal_length) / (curr_sampling_freq)
+    upsampled_signal = np.zeros(desired_n_of_samples)
     upsampled_signal = sp.signal.resample(signal, desired_n_of_samples)
 
     return upsampled_signal
