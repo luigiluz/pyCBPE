03e650879be77d4f996cbf265c6a06d95b70deb9
Luigi Luz <lfml@cesar.org.br>
2020-12-14 01:13:47 -0300

    Add fixes for preprocessing module

--- cbpe-python/preprocessing.py
+++ cbpe-python/preprocessing.py
@@ -1,49 +1,57 @@
 """ This is the package responsible for realizing the preprocessment of
 photoplethysmogram signals. """
 
-import scipy as sp
+import numpy as np
+
+from scipy import signal
+
 
 def preprocess(signal):
     preprocessed_signal = _remove_high_frequency_components(signal)
-    preprocessed_signal = _remove_baseline_wander(signal)
-    preprocessed_signal = _upsample(signal)
+    preprocessed_signal = _remove_baseline_wander(preprocessed_signal)
+    preprocessed_signal = _upsample(preprocessed_signal)
 
     return preprocessed_signal
 
 
-def _remove_high_frequency_components(signal):
+def _remove_high_frequency_components(ppg_signal):
     sampling_freq = 125
     filter_order = 5
     max_ripple = 0.5
     cut_off_freq_hz = 10
-    cut_off_freq_rad_smp = (2 * cut_off_freq)/sampling_freq
-    low_pass_filtered_signal = np.zeros(length(signal))
+    cut_off_freq_rad_smp = (2 * cut_off_freq_hz)/sampling_freq
+    low_pass_filtered_signal = np.zeros(len(ppg_signal))
 
-    cheby_num, cheby_den = sp.signal.cheby1(filter_order, max_ripple, cut_off_freq_rad_smp, btype='lowpass')
-    low_pass_filtered_signal = sp.signal.filtfilt(cheby_num, cheby_den, signal)
+    cheby_num, cheby_den = signal.cheby1(filter_order, max_ripple, cut_off_freq_rad_smp, btype='lowpass')
+    low_pass_filtered_signal = signal.filtfilt(cheby_num, cheby_den, ppg_signal)
 
     return low_pass_filtered_signal
 
 
-def _remove_baseline_wander(signal):
-    signal_length = len(signal)
-    first_window_size = np.floor(0.7 * signal_length)
-    second_window_size = np.floor(0.3 * signal_length)
+def _remove_baseline_wander(ppg_signal):
+    signal_length = len(ppg_signal)
+    first_window_size = int(np.floor(0.7 * signal_length))
+    second_window_size = int(np.floor(0.3 * signal_length))
+
+    mov_median_output = np.zeros(signal_length)
     detrended_signal = np.zeros(signal_length)
 
-    detrended_signal = sp.signal.medfilt(signal, first_window_size)
-    detrended_signal = sp.signal.medfilt(detrended_signal, second_window_size)
+    # todo: make sure that window sizes are odd
+    mov_median_output = signal.medfilt(ppg_signal, first_window_size)
+    mov_median_output = signal.medfilt(mov_median_output, second_window_size)
+
+    detrended_signal = np.subtract(ppg_signal, mov_median_output)
 
     return detrended_signal
 
 
-def _upsample(signal):
-    signal_length = len(signal)
+def _upsample(ppg_signal):
+    signal_length = len(ppg_signal)
     curr_sampling_freq = 125
     desired_sampling_freq = 500
 
-    desired_n_of_samples = (desired_sampling_freq * signal_length) / (curr_sampling_freq)
+    desired_n_of_samples = int((desired_sampling_freq * signal_length) / (curr_sampling_freq))
     upsampled_signal = np.zeros(desired_n_of_samples)
-    upsampled_signal = sp.signal.resample(signal, desired_n_of_samples)
+    upsampled_signal = signal.resample(ppg_signal, desired_n_of_samples)
 
     return upsampled_signal
