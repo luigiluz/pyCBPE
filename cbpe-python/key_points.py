a3e4ede5832415214b9943a8ee07363a95ca6acf
Luigi Luz <lfml@cesar.org.br>
2020-12-28 00:48:48 -0300

    Add key_points module

--- cbpe-python/key_points.py
+++ cbpe-python/key_points.py
@@ -24,170 +24,222 @@ PULSE_EVAL = "pulse_eval"
 
 
 def extract(normalized_pulse):
-	# group all key points in this dictionary
-	key_points = {
-		SYS_PEAK: 0,
-		MAX_SLP: 0,
-		DIAS_PEAK: 0,
-		DIC_NOTCH: 0,
-		INFL_POINT: 0
-	}
+    # group all key points in this dictionary
+    key_points = {
+        SYS_PEAK: 0,
+        MAX_SLP: 0,
+        DIAS_PEAK: 0,
+        DIC_NOTCH: 0,
+        INFL_POINT: 0
+    }
 
-	key_points[SYS_PEAK] = _find_systolic_peak_location(normalized_pulse)
+    key_points[SYS_PEAK] = _find_systolic_peak_location(normalized_pulse)
 
-	regular_pulse, pulse_first_derivative, pulse_second_derivative = _fit_section_polynoms(normalized_pulse, key_points[SYS_PEAK])
+    regular_pulse, pulse_first_derivative, pulse_second_derivative = _fit_section_polynoms(normalized_pulse, key_points[SYS_PEAK])
 
-	key_points[MAX_SLP] = _find_max_slope(pulse_first_derivative, key_points)
+    key_points[MAX_SLP] = _find_max_slope(pulse_first_derivative, key_points)
+    key_points[DIAS_PEAK] = _find_diastolic_peak(pulse_first_derivative, pulse_second_derivative, key_points)
+    key_points[DIC_NOTCH] = _find_dicrotic_notch(pulse_second_derivative, key_points)
+    key_points[INFL_POINT] = _find_inflection_poiint(pulse_second_derivative, key_points)
 
-	# key_points["diastolic peak"] = _find_diastolic_peak()
-	# key_points["dicrotic notch"] = _find_dicrotic_notch()
-	# key_points["inflection point"] = "find_inflection_point()"
+    return key_points
 
 
 def _find_systolic_peak_location(normalized_pulse):
-	systolic_peak = pyampd.ampd.find_peaks(normalized_pulse)
+    systolic_peak = 0
 
-	return systolic_peak
+    peaks = pyampd.ampd.find_peaks(normalized_pulse)
+    if len(peaks) == 0:
+        return systolic_peak
+
+    systolic_peak = peaks[0]
+
+    return systolic_peak
 
 
 def _fit_section_polynoms(normalized_pulse, systolic_peak):
-	sampling_freq = 125
-	pulse_length = len(normalized_pulse)
-	pulse_time = np.arange(0, pulse_length / sampling_freq, 1 / sampling_freq)
-	# prealocation
-	regular_pulse = {
-		ASC_POL : np.zeros(ASCENDING_POL_N_OF_COEFS), # add this to a constant
-		DESC_POL : np.zeros(DESCENDING_POL_N_OF_COEFS), # add this to a constant
-		ASC_SEC_EVAL : np.zeros(ascending_len),
-		DESC_SEC_EVAL : np.zeros(descending_len),
-		PULSE_EVAL: np.zeros(ascending_len + descending_len)
-	}
+    sampling_freq = 125
+    pulse_length = len(normalized_pulse)
+    pulse_time = np.arange(0, pulse_length / (4*sampling_freq), 1 / (4*sampling_freq))
 
-	pulse_first_derivative = {
-		ASC_POL : np.zeros(ASCENDING_POL_N_OF_COEFS), # add this to a constant
-		DESC_POL : np.zeros(DESCENDING_POL_N_OF_COEFS), # add this to a constant
-		ASC_SEC_EVAL : np.zeros(ascending_len),
-		DESC_SEC_EVAL : np.zeros(descending_len),
-		PULSE_EVAL: np.zeros(ascending_len + descending_len)
-	}
+    ascending_section, descending_section = _separate_pulse_in_sections(normalized_pulse, systolic_peak)
 
-	pulse_second_derivative = {
-		ASC_POL : np.zeros(ASCENDING_POL_N_OF_COEFS), # add this to a constant
-		DESC_POL : np.zeros(DESCENDING_POL_N_OF_COEFS), # add this to a constant
-		ASC_SEC_EVAL : np.zeros(ascending_len),
-		DESC_SEC_EVAL : np.zeros(descending_len),
-		PULSE_EVAL: np.zeros(ascending_len + descending_len)
-	}
+    ascending_len = len(ascending_section)
+    descending_len = len(descending_section)
 
-	ascending_section, descending_section = _separate_pulse_in_sections(normalized_pulse, systolic_peak)
+    # prealocation
+    regular_pulse = {
+        ASC_POL : np.zeros(ASCENDING_POL_N_OF_COEFS),
+        DESC_POL : np.zeros(DESCENDING_POL_N_OF_COEFS),
+        ASC_SEC_EVAL : np.zeros(ascending_len),
+        DESC_SEC_EVAL : np.zeros(descending_len),
+        PULSE_EVAL: np.zeros(ascending_len + descending_len)
+    }
 
-	ascending_len = len(ascending_section)
-	descending_len = len(descending_section)
+    pulse_first_derivative = {
+        ASC_POL : np.zeros(ASCENDING_POL_N_OF_COEFS),
+        DESC_POL : np.zeros(DESCENDING_POL_N_OF_COEFS),
+        ASC_SEC_EVAL : np.zeros(ascending_len),
+        DESC_SEC_EVAL : np.zeros(descending_len),
+        PULSE_EVAL: np.zeros(ascending_len + descending_len)
+    }
 
-	ascending_time = pulse_time[0 : systolic_peak]
-	descending_time = pulse_time[systolic_peak + 1 : -1]
+    pulse_second_derivative = {
+        ASC_POL : np.zeros(ASCENDING_POL_N_OF_COEFS),
+        DESC_POL : np.zeros(DESCENDING_POL_N_OF_COEFS),
+        ASC_SEC_EVAL : np.zeros(ascending_len),
+        DESC_SEC_EVAL : np.zeros(descending_len),
+        PULSE_EVAL: np.zeros(ascending_len + descending_len)
+    }
 
-	###### without derivative ######
-	# fit sections polynoms
-	regular_pulse[ASC_POL] = np.polyfit(ascending_time, ascending_section, deg=5)
-	regular_pulse[DESC_POL] = np.polyfit(descending_time, descending_section, deg=7)
-	# evaluate sections polynoms
-	regular_pulse[ASC_SEC_EVAL] = np.polyval(regular_pulse[ASC_POL], ascending_time)
-	regular_pulse[DESC_SEC_EVAL] = np.polyval(regular_pulse[DESC_POL], descending_time)
-	# concatenate sections polynoms
-	regular_pulse[PULSE_EVAL] = np.concatenate((regular_pulse[ASC_SEC_EVAL], regular_pulse[DESC_SEC_EVAL]))
+    ascending_time = pulse_time[0:systolic_peak]
+    descending_time = pulse_time[systolic_peak + 1 : -1]
 
-	###### first derivative ######
-	# fit sections first derivatives polynoms
-	pulse_first_derivative[ASC_POL] = np.polyder(ascending_polynom)
-	pulse_first_derivative[DESC_POL] = np.polyder(descending_polynom)
-	# evaluate sections first derivatives polynoms
-	pulse_first_derivative[ASC_SEC_EVAL] = np.polyval(pulse_first_derivative[ASC_POL], ascending_time)
-	pulse_first_derivative[DESC_SEC_EVAL] = np.polyval(pulse_first_derivative[DESC_POL], descending_time)
-	# concatenate first derivates sections polynoms
-	pulse_first_derivative[PULSE_EVAL] = np.concatenate((pulse_first_derivative[ASC_SEC_EVAL], pulse_first_derivative[DESC_SEC_EVAL]))
+    ###### without derivative ######
+    # fit sections polynoms
+    regular_pulse[ASC_POL] = np.polyfit(ascending_time, ascending_section, deg=5)
+    regular_pulse[DESC_POL] = np.polyfit(descending_time, descending_section, deg=7)
+    # evaluate sections polynoms
+    regular_pulse[ASC_SEC_EVAL] = np.polyval(regular_pulse[ASC_POL], ascending_time)
+    regular_pulse[DESC_SEC_EVAL] = np.polyval(regular_pulse[DESC_POL], descending_time)
+    # concatenate sections polynoms
+    regular_pulse[PULSE_EVAL] = np.concatenate((regular_pulse[ASC_SEC_EVAL], regular_pulse[DESC_SEC_EVAL]))
 
-	###### second derivative ######
-	# fit sections second derivatives polynoms
-	pulse_second_derivative[ASC_POL] = np.polyder(ascending_pol_first_der)
-	pulse_second_derivative[DESC_POL] = np.polyder(descending_pol_first_der)
-	# evaluate sections second derivatives polynoms
-	pulse_second_derivative[ASC_SEC_EVAL] = np.polyval(pulse_second_derivative[ASC_POL], ascending_time)
-	pulse_second_derivative[DESC_SEC_EVAL] = np.polyval(pulse_second_derivative[DESC_POL], descending_time)
-	# concatenate second derivatives sections polynoms
-	pulse_second_derivative[PULSE_EVAL] = np.concatenate((pulse_second_derivative[ASC_SEC_EVAL], pulse_second_derivative[DESC_SEC_EVAL]))
+    ###### first derivative ######
+    # fit sections first derivatives polynoms
+    pulse_first_derivative[ASC_POL] = np.polyder(regular_pulse[ASC_POL])
+    pulse_first_derivative[DESC_POL] = np.polyder(regular_pulse[DESC_POL])
+    # evaluate sections first derivatives polynoms
+    pulse_first_derivative[ASC_SEC_EVAL] = np.polyval(pulse_first_derivative[ASC_POL], ascending_time)
+    pulse_first_derivative[DESC_SEC_EVAL] = np.polyval(pulse_first_derivative[DESC_POL], descending_time)
+    # concatenate first derivates sections polynoms
+    pulse_first_derivative[PULSE_EVAL] = np.concatenate((pulse_first_derivative[ASC_SEC_EVAL], pulse_first_derivative[DESC_SEC_EVAL]))
 
-	return regular_pulse, pulse_first_derivative, pulse_second_derivative
+    ###### second derivative ######
+    # fit sections second derivatives polynoms
+    pulse_second_derivative[ASC_POL] = np.polyder(pulse_first_derivative[ASC_POL])
+    pulse_second_derivative[DESC_POL] = np.polyder(pulse_first_derivative[DESC_POL])
+    # evaluate sections second derivatives polynoms
+    pulse_second_derivative[ASC_SEC_EVAL] = np.polyval(pulse_second_derivative[ASC_POL], ascending_time)
+    pulse_second_derivative[DESC_SEC_EVAL] = np.polyval(pulse_second_derivative[DESC_POL], descending_time)
+    # concatenate second derivatives sections polynoms
+    pulse_second_derivative[PULSE_EVAL] = np.concatenate((pulse_second_derivative[ASC_SEC_EVAL], pulse_second_derivative[DESC_SEC_EVAL]))
+
+    return regular_pulse, pulse_first_derivative, pulse_second_derivative
 
 
 # todo: change this to receive key_points dict
 def _separate_pulse_in_sections(normalized_pulse, systolic_peak):
-	ascending_section = normalized_pulse[0 : systolic_peak]
-	descending_section = normalized_pulse[systolic_peak + 1 : -1]
+    ascending_section = normalized_pulse[0 : systolic_peak]
+    descending_section = normalized_pulse[systolic_peak + 1 : -1]
 
-	return ascending_section, descending_section
+    return ascending_section, descending_section
 
 
 def _find_max_slope(pulse_first_derivative, key_points):
-	max_slope = 0 # always returns 0 if the key point hasn't been detected
-	# max slope is the point where the first derivative has its largest value
-	# it happens BEFORE systolic peak
+    max_slope = 0 # always returns 0 if the key point hasn't been detected
 
-	maximals = pyampd.ampd.find_peaks(pulse_first_derivative[PULSE_EVAL])
-	maximals_before_sys_peak = maximals[maximals < key_points[SYS_PEAK]]
+    maximals = pyampd.ampd.find_peaks(pulse_first_derivative[PULSE_EVAL])
+    maximals_before_sys_peak = maximals[maximals < key_points[SYS_PEAK]]
 
-	if (len(maximals_before_sys_peak) > 1):
-		max_slope = maximals_before_sys_peak[0]
+    if (len(maximals_before_sys_peak) > 0):
+        max_slope = maximals_before_sys_peak[0]
 
-	return max_slope
+    return max_slope
 
-# entradas necessarias:
-# first_derivative_eval
-# first_derivative_pol
-# second_derivative_eval
-# systolic_peak
+
 def _find_diastolic_peak(pulse_first_derivative, pulse_second_derivative, key_points):
-	# the point at which the first derivative of the polynomial is equal to zero
-	# and the second derivative is negative
-	# it happens AFTER systolic peak
-	# if there is no such point
-	# then the point at which the second derivative is a local minimum is chosen as the diastolic peak
+    fs = 125
+    ts = np.round(1/(4*fs), 3)
+    t = np.arange(0, len(pulse_second_derivative[PULSE_EVAL]) * ts, ts)
 
-	diastolic_peak = 0 # always returns 0 if the key point hasn't been detected
+    diastolic_peak = 0 # always returns 0 if the key point hasn't been detected
 
-	# note: consider using np.roots(p) to find the roots of the polynom
+    second_derivative_minimals = pyampd.ampd.find_peaks(-pulse_second_derivative[PULSE_EVAL])
+    second_derivative_min_after_sys_peak = second_derivative_minimals[second_derivative_minimals > key_points[SYS_PEAK]]
 
-# entradas necessarias:
-# systolic_peak
-# diastolic_peak
-# second_derivative_eval
+    if len(second_derivative_minimals > 0):
+        # Validate if we should get the first or the last minimal
+        diastolic_peak = second_derivative_min_after_sys_peak[0]
+
+    first_derivative_roots = np.roots(pulse_first_derivative[DESC_POL])
+    positive_roots = first_derivative_roots[first_derivative_roots > 0]
+
+    is_complex = np.iscomplex(positive_roots)
+    if all(is_complex):
+        return diastolic_peak
+
+    is_real = np.isreal(positive_roots)
+    real_roots = positive_roots[is_real]
+    first_real_root = real_roots[0]
+    min_root = first_real_root - ts
+    max_root = first_real_root + ts
+
+    is_greater_than_min = t > min_root
+    is_less_than_max = t < max_root
+
+    value_index = is_greater_than_min & is_less_than_max
+    if not any(value_index):
+        return diastolic_peak
+
+    if pulse_first_derivative[PULSE_EVAL][value_index] and (pulse_second_derivative[PULSE_EVAL][value_index] < 0):
+        diastolic_peak = np.where(t == t[value_index])
+        return diastolic_peak
+
+    return diastolic_peak
+
+
 def _find_dicrotic_notch(pulse_second_derivative, key_points):
-	dicrotic_notch = 0 # always returns 0 if the key point hasn't been detected
-	# is located BEFORE diastolic peak and AFTER systolic peak
-	# the point where the second derivative of the PPG signal is a local maximum
+    dicrotic_notch = 0 # always returns 0 if the key point hasn't been detected
 
-	second_derivative_maximals = pyampd.ampd.find_peaks(pulse_second_derivative[PULSE_EVAL])
-	after_sys_peak = second_derivative_maximals > key_points[SYS_PEAK]
-	before_dias_peak = second_derivative_maximals < key_points[DIAS_PEAK]
+    second_derivative_maximals = pyampd.ampd.find_peaks(pulse_second_derivative[PULSE_EVAL])
+    after_sys_peak = second_derivative_maximals > key_points[SYS_PEAK]
+    before_dias_peak = second_derivative_maximals < key_points[DIAS_PEAK]
 
-	second_derivative_maximals_between_sys_and_dias_peak = after_sys_peak & before_dias_peak
-	if (len(second_derivative_maximals_between_sys_and_dias_peak) > 1):
-		dicrotic_notch = second_derivative_maximals_between_sys_and_dias_peak[0]
+    second_derivative_maximals_between_sys_and_dias_peak = second_derivative_maximals[after_sys_peak & before_dias_peak]
+    if (len(second_derivative_maximals_between_sys_and_dias_peak) > 0):
+        dicrotic_notch = second_derivative_maximals_between_sys_and_dias_peak[0]
 
-	return dicrotic_notch
+    return dicrotic_notch
 
-# entradas necessarias:
-# dicrotic_notch
-# diastolic_peak
-# second_derivative_eval
-# second_derivative_pol
-def _find_inflection_poiint():
-	# it lies between the dicrotic notch and diastolic peak
-	# it is located AFTER dicrotic notch and BEFORE diastolic peak
-	# at this point, the second derivative of the PPG signal is equal to zero
-	# if no such point exists, the inflection point is chosen to be
-	# the midpoint between the dicrotic notch and the diastolic peak
 
-	inflection_point = 0 # always returns 0 if the key point hasn't been detected
+def _find_inflection_poiint(pulse_second_derivative, key_points):
+    fs = 125
+    ts = np.round(1/(4*fs), 3)
+    t = np.arange(0, len(pulse_second_derivative[PULSE_EVAL]) * ts, ts)
 
-	# note: consider using np.roots(p) to find the roots of the polynom
+    inflection_point = 0 # always returns 0 if the key point hasn't been detected
+
+    inflection_point = int(np.floor((key_points[DIC_NOTCH] + key_points[DIAS_PEAK]) / 2))
+
+    second_derivative_roots = np.roots(pulse_second_derivative[DESC_POL])
+    positive_roots = second_derivative_roots[second_derivative_roots > 0]
+
+    roots_after_dic_notch = positive_roots > t[key_points[DIC_NOTCH]]
+    roots_before_dias_peak = positive_roots < t[key_points[DIAS_PEAK]]
+
+    roots_in_interval = positive_roots[roots_after_dic_notch & roots_before_dias_peak]
+
+    is_complex = np.iscomplex(roots_in_interval)
+    if all(is_complex):
+        return inflection_point
+
+    is_real = np.isreal(roots_in_interval)
+    real_roots = roots_in_interval[is_real]
+    if len(real_roots == 0):
+        return inflection_point
+
+    first_real_root_in_interval = real_roots[0]
+    min_root = first_real_root_in_interval - ts
+    max_root = first_real_root_in_interval + ts
+
+    is_greater_than_min = t > min_root
+    is_less_than_max = t < max_root
+
+    is_in_interval = is_greater_than_min & is_less_than_max & after_dic_notch & before_dias_peak
+    if not any(is_in_interval):
+        return inflection_point
+
+    inflection_point = np.where(t == t[is_in_interval])
+
+    return inflection_point
--- cbpe-python/tester.py
+++ cbpe-python/tester.py
@@ -9,6 +9,13 @@ import scipy as sp
 # Package modules
 import preprocessing
 import normalization
+import key_points
+
+SYS_PEAK = "systolic peak"
+MAX_SLP = "max slope"
+DIAS_PEAK = "diastolic peak"
+DIC_NOTCH = "dicrotic notch"
+INFL_POINT = "inflection point"
 
 ROOT_PATH = "/home/lfml-cesar/Documents/UACSA/cbpe-python"
 
@@ -28,25 +35,35 @@ def main():
     normalized_ppg_pulse = normalization.normalize(preprocessed_ppg)
     t_norm = np.arange(0, len(normalized_ppg_pulse)/(4*sampling_freq), 1/(4*sampling_freq))
 
+    print("### Run key_points ###")
+    key_points_loc = key_points.extract(normalized_ppg_pulse)
+
     f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 
     ax1.plot(t, ppg_seg)
     ax1.set_xlabel('Tempo (seg)')
     ax1.set_title('Sinal puro')
-    #ax1.xlabel("Tempo (seg)")
-    #ax1.ylabel("PPG Amplitude")
-    #ax1.show()
 
     ax2.plot(t_pp, preprocessed_ppg)
     ax2.set_xlabel('Tempo (seg)')
     ax2.set_title('Sinal preprocessado')
-    #ax2.xlabel("Tempo (seg)")
-    #ax2.ylabel("PPG Amplitude")
-    #ax2.show()
-
     plt.show()
 
     plt.plot(t_norm, normalized_ppg_pulse)
+    plt.plot(t_norm[key_points_loc[SYS_PEAK]], normalized_ppg_pulse[key_points_loc[SYS_PEAK]], marker='o')
+    plt.annotate("Systolic peak", xy=(t_norm[key_points_loc[SYS_PEAK]], normalized_ppg_pulse[key_points_loc[SYS_PEAK]]))
+
+    plt.plot(t_norm[key_points_loc[MAX_SLP]], normalized_ppg_pulse[key_points_loc[MAX_SLP]], marker='o', label='Max slope')
+    plt.annotate("Max slope", xy=(t_norm[key_points_loc[MAX_SLP]], normalized_ppg_pulse[key_points_loc[MAX_SLP]]))
+
+    plt.plot(t_norm[key_points_loc[DIAS_PEAK]], normalized_ppg_pulse[key_points_loc[DIAS_PEAK]], marker='o', label='Diastolic peak')
+    plt.annotate("Diastolic peak", xy=(t_norm[key_points_loc[DIAS_PEAK]], normalized_ppg_pulse[key_points_loc[DIAS_PEAK]]))
+
+    plt.plot(t_norm[key_points_loc[INFL_POINT]], normalized_ppg_pulse[key_points_loc[INFL_POINT]], marker='o', label='Inflection point')
+    plt.annotate("Inflection point", xy=(t_norm[key_points_loc[INFL_POINT]], normalized_ppg_pulse[key_points_loc[INFL_POINT]]))
+
+    plt.plot(t_norm[key_points_loc[DIC_NOTCH]], normalized_ppg_pulse[key_points_loc[DIC_NOTCH]], marker='o', label='Dicrotic notch')
+    plt.annotate("Dicrotic notch", xy=(t_norm[key_points_loc[DIC_NOTCH]], normalized_ppg_pulse[key_points_loc[DIC_NOTCH]]))
 
     plt.show()
 
