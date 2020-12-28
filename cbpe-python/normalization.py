Add minor fix in normalization module

cbpe-python/normalization.py
@@ -9,8 +9,9 @@ from pyampd.ampd import find_peaks
 def normalize(signal):
     minimals = _detect_minimals(signal)
     central_pulse = _get_central_pulse(signal, minimals)
-    if not central_pulse
-        normalized_pulse = []
+    is_central_pulse_empty = central_pulse.size == 0
+    if is_central_pulse_empty:
+        normalized_pulse = np.array([])
         return normalized_pulse
 
     normalized_pulse = _normalize_pulse(central_pulse)
@@ -26,8 +27,8 @@ def _detect_minimals(signal):
 
 def _get_central_pulse(signal, minimals):
     n_of_minimals = len(minimals)
-    if n_of_minimals < 2
-        central_pulse = []
+    if n_of_minimals < 2:
+        central_pulse = np.array([])
         return central_pulse
 
     central_minimal = int(np.floor(n_of_minimals / 2))